fa8643b2ebc422690978f494c61a3f93f1db27f3
YuriFont <yurifontenelesilva180303@gmail.com>
2023-10-18 14:44:47 -0300

    add atoi

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 12:48:01 by yufonten          #+#    #+#             */
-/*   Updated: 2023/10/18 10:13:23 by yufonten         ###   ########.fr       */
+/*   Updated: 2023/10/18 14:11:26 by yufonten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,10 @@ char	*ft_strrchr(const char *s, int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+
+int		ft_atoi(const char *nptr);
 
 void	ft_putchar_fd(char c, int fd);
 
