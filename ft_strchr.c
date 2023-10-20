d4c2f7734088b4b00fbc87c2b6a61b0c29ed3dbb
YuriFont <yurifontenelesilva180303@gmail.com>
2023-10-19 22:09:25 -0300

    mod strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: yufonten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 13:49:51 by yufonten          #+#    #+#             */
-/*   Updated: 2023/10/17 14:31:08 by yufonten         ###   ########.fr       */
+/*   Updated: 2023/10/19 22:08:15 by yufonten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,16 @@ char	*ft_strchr(const char *s, int c)
 {
 	int	i;
 
-	i = 0;
 	if (!s)
 		return (NULL);
+	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
+		if (s[i] == (char)c)
 			return ((char *)(s + i));
 		i++;
 	}
+	if (s[i] == (char)c)
+		return ((char *)(s + i));
 	return (NULL);
 }
